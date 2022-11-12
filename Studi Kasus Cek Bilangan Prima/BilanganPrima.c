2a1c2d44cfe28020d7f5e965c72eec11e13fb714
Irhan Hisyam <108678365+irhanhisyamdwinugroho@users.noreply.github.com>
2022-11-12 15:28:04 +0800

    Add files via upload

---
+++ Studi Kasus Cek Bilangan Prima/BilanganPrima.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+  printf("## Program Bahasa C Persegi Angka ## \n");
+  printf("==================================== \n\n");
+ 
+  int besar_persegi,i,j;
+ 
+  printf("Input besar persegi: ");
+  scanf("%d",&besar_persegi);
+ 
+  printf("\n");
+ 
+  for(i=1; i<=besar_persegi; i++) {
+    for(j=1; j<=besar_persegi; j++) {
+      printf("%d ",i);
+    }
+    printf("\n");
+  }
+ 
+  return 0;
+}
